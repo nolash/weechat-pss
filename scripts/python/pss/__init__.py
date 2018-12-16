2a56bc8816b1c35b5eed1f56a2d3529052060e49
nolash <dev@holbrook.no>
2018-12-16 23:01:10 +0100

    Rename lib files

--- scripts/python/pss/__init__.py
+++ scripts/python/pss/__init__.py
@@ -4,5 +4,5 @@ from content import Stream, rpc_parse
 from tools import *
 from error import *
 from message import *
-from feed import *
 from bzz import *
+from agent import *
---
+++ scripts/python/pss/agent.py
@@ -0,0 +1,71 @@
+import urllib2
+import copy
+import re
+import os
+import select
+
+regexStatusLine = re.compile("^HTTP/1.1 (\d{3}) (.+)\n", re.M)
+
+
+class Agent:
+	sock = None
+	up = False
+	basereq = None
+	host = "localhost"
+	port = "8546"	
+
+
+	
+	def __init__(self, host, port, sock):
+		# common request params
+		self.sock = sock	
+		self.host = host
+		self.port = str(port)
+
+		self.basereq = urllib2.Request("http://" + host + ":" + str(port) + "/")
+		self.basereq.add_header("Connection", "keep-alive")
+		self.basereq.add_header("Content-type", "application/x-www-form-urlencoded")
+		self.basereq.add_header("Accept", "*/*")
+			
+
+
+	def new_request(self):
+		req = urllib2.Request("http://" + self.host + ":" + self.port + "/")
+		req.add_header("Connection", "keep-alive")
+		req.add_header("Content-type", "application/x-www-form-urlencoded")
+		req.add_header("Accept", "*/*")
+		return req
+
+
+	def send(self, path, data, querystring=""):
+		req = self.new_request()
+		req.add_header("Content-length", str(len(data)))
+		req.add_data(data)
+		
+		requeststring = req.get_method() + " " + path
+		if querystring != "":
+			requeststring += "?" + querystring
+		requeststring += " HTTP/1.1\nHost: " + req.get_host() + "\n"
+		for (k, v) in req.header_items():
+			requeststring += k + ": " + v + "\n"
+		requeststring += "\n" + req.get_data()	
+		print requeststring
+		#self.sock.send(requeststring)
+		select.select([], [self.sock], [])
+		os.write(self.sock, requeststring)
+		#r = self.sock.recv(1024)
+		select.select([self.sock], [], [])
+		r = os.read(self.sock, 1024)
+		m = regexStatusLine.match(r)
+		if m.group(1) != "200":
+			print r
+			raise Exception("HTTP send to swarm failed: " + str(m.group(0)))
+
+		body = ""
+		try:
+			_, body  = r.split("\x0d\x0a\x0d\x0a")
+		except:
+			_, body = r.split("\x0a\x0a")
+
+		return body
+	
