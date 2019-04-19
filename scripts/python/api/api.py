b3322e6e1036b0a8bf159809d773bd716d8abeb8
nolash <dev@holbrook.no>
2019-04-19 23:37:07 +0200

    Add outqueue handler with thread (simple echo for now)

--- scripts/python/api/api.py
+++ scripts/python/api/api.py
@@ -37,12 +37,11 @@ class ApiParser:
 		if len(data) == 0:
 			return (None, None)
 		if self.item == None:
-			itemid = (data[0] & 31) << 7
+			itemid = (data[0] & 31) << 8
 			itemid += data[1]
 			#err = data[0] >> 5
 			self.item = ApiItem(itemid)
 			self.remaining = struct.unpack(">I", data[3:7])[0]
-			print("got id {} len {}\n".format(itemid, self.remaining))
 			self.item.data += data[:7]
 			data = data[7:]
 
@@ -64,8 +63,8 @@ class ApiServer:
 	def __init__(self):
 		self.agent = pss.Agent("127.0.0.1", 8500)
 		self.bzz = pss.Bzz(self.agent)
-		self.queue_i = pss.Queue(4095)
-		self.queue_o = pss.Queue(4095)
+		self.queue_i = pss.Queue((1<<13)-1)
+		self.queue_o = pss.Queue((1<<13)-1)
 		self.lock_i = threading.Lock() 
 		self.lock_o = threading.Lock()
 		self.lock_main = threading.Lock()
@@ -75,15 +74,27 @@ class ApiServer:
 		self.sockaddr = "tmp_{:}.sock".format(uuid.uuid4())
 		self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
 		self.sock.bind(self.sockaddr)
-		self.thread_handle = threading.Thread(None, self.handle_in, "handle")
-		self.thread_handle.start()
-
+		self.thread_in = threading.Thread(None, self.handle_in, "handle_in")
+		self.thread_in.start()
 
 	def __del__(self):
-		print("closing")
 		self.lock_main.acquire()
 		self.agent.close()
 		self.lock_main.release()
+
+
+	def handle_out(self, sock):
+		while True:
+			self.lock_o.acquire()
+			item = self.queue_o.get()
+			self.lock_o.release()
+			if item != None:
+				select.select([], [sock.fileno()], [])
+				sock.send(item.data)
+			elif not self.running:
+				return
+			time.sleep(0.1)	
+		pass
 
 
 	def process(self):
@@ -93,32 +104,68 @@ class ApiServer:
 			self.lock_i.release()
 			if item != None: 
 				data = item.data
+				typ = ""
+				bank = False
+				multi = False
 				sys.stdout.write("data {}".format(data[7:]))
 				if _flag_ctx_peer & data[2] > 0:
-					sys.stdout.write("|peer")
+					typ = "peer"
 				elif _flag_ctx_comm & data[2] > 0:
-					sys.stdout.write("|comm")
+					typ = "comm"
 				elif _flag_ctx_content & data[2] > 0:
-					sys.stdout.write("|content")
+					typ = "content"
 				elif _flag_ctx_tag & data[2] > 0:
-					sys.stdout.write("|tag")
+					typ = "tag"	
 				if _flag_ctx_bank & data[2] > 0:
-					sys.stdout.write("|bank")
+					bank = True	
 				if _flag_ctx_multi & data[2] > 0:
-					sys.stdout.write("|multi")
-				print()
-				#print("ok: {}".format(self.bzz.add(data)))
+					multi = True
+				# short circuit
+				h = self.newheader(item.id, 1, typ, item.data, bank, multi)
+				outitem = ApiItem(item.id)
+				print("hhdd", hex(h[0]), hex(h[1]), hex(h[2]))
+				h += item.data[7:]
+				outitem.data =  h
+				self.lock_o.acquire()
+				self.queue_o.add(outitem)
+				self.lock_o.release()
 			elif not self.running:
 				return
 			time.sleep(0.1)
-		
+
+
+	def newheader(self, seq, err, typ, data, bank=False, multi=False):
+		h = bytearray(7)
+		h[0] = err << 5
+		h[0] |= (seq >> 8) & 0x1f 
+		print("hh", h[0])
+		h[1] = seq & 0xff
+		if typ == "comm":
+			h[2] = _flag_ctx_comm
+		elif typ == "peer":
+			h[2] = _flag_ctx_peer
+		elif typ == "tag":
+			h[2] = _flag_ctx_tag
+		elif typ == "content":
+			h[2] = _flag_ctx_content
+		if bank:
+			h[2] |= 0x40
+		if multi:
+			h[2] |= 0x80
+		(lenbytes) = struct.pack(">I", len(data)-7)
+		i = 3
+		for l in lenbytes:
+			h[i] = l
+			i+=1
+		return h
+	
 	
 	def handle_in(self):
-		#print("start listen {}\n".format(self.sock))
 		self.sock.listen(0)
 		(c, addr) = self.sock.accept()
 		c.settimeout(0.1)
-		#print("got incoming addr:{} sock:{}\n".format(addr, c))
+		self.thread_out = threading.Thread(None, self.handle_out, "handle_out", [c])
+		self.thread_out.start()
 		parser = ApiParser()
 		while self.running:
 			data = ""
@@ -128,13 +175,11 @@ class ApiServer:
 			except:
 				print("sock timeout\n")
 				continue
-			print("in: {}".format(data))
 			(complete, leftovers) = parser.put(data)
 			while leftovers != None:
 				self.lock_i.acquire()
 				self.queue_i.add(complete)
 				self.lock_i.release()
-				print("complete {}".format(complete))
 				(complete, leftovers) = parser.put(leftovers)
 		c.close()
 		self.sock.close()
@@ -151,6 +196,7 @@ class ApiServer:
 		self.lock_main.acquire()
 		self.running = False
 		self.lock_main.release()
-		self.thread_handle.join()
+		self.thread_in.join()
+		self.thread_out.join()
 		self.thread_process.join()
 
--- scripts/python/server_test.py
+++ scripts/python/server_test.py
@@ -24,12 +24,37 @@ class TestServer(unittest.TestCase):
 	def test_basic(self):
 		self.obj.connect(self.sock)
 		print("sending on addr:{} sock:{}\n".format(self.obj.sockaddr, self.sock))
+
+		# basic check for echo
 		select.select([], [self.fileno], [])
-		self.sock.send(b"\x00\x08\x01\x00\x00\x00\x03\x66\x6f\x6f")
+		datasend = b"\x00\x08\x01\x00\x00\x00\x03\x66\x6f\x6f"
+		self.sock.send(datasend)
+		select.select([self.fileno], [], [])
+		datarecv = self.sock.recv(1024)
+		self.assertEqual(datasend[1:], datarecv[1:])
+		self.assertEqual(datasend[0], datarecv[0] & 0x1f)
+		self.assertEqual(0x20, datarecv[0] & 0xe0)
+
+		# check that the error (erroneously) sent does not come back
 		select.select([], [self.fileno], [])
-		self.sock.send(b"\x30\x00\x42\x00\x00\x00\x04\x62\x61\x72\x20")
+		datasend = b"\x70\x00\x42\x00\x00\x00\x04\x62\x61\x72\x20"
+		self.sock.send(datasend)
+		select.select([self.fileno], [], [])
+		datarecv = self.sock.recv(1024)
+		self.assertEqual(datasend[1:], datarecv[1:])
+		self.assertEqual(datasend[0] & 0x3f, datarecv[0])
+		self.assertEqual(0x20, datarecv[0] & 0xe0)
+
+		# check correct treatment of high order byte in id
 		select.select([], [self.fileno], [])
-		self.sock.send(b"\x10\x01\x88\x00\x00\x00\x05\x78\x79\x7a\x7a\x79")
+		datasend = b"\x10\x01\x88\x00\x00\x00\x05\x78\x79\x7a\x7a\x79"
+		self.sock.send(datasend)
+		select.select([self.fileno], [], [])
+		datarecv = self.sock.recv(1024)
+		self.assertEqual(datasend[1:], datarecv[1:])
+		self.assertEqual(datasend[0], datarecv[0] & 0x1f)
+		self.assertEqual(0x20, datarecv[0] & 0xe0)
+
 		time.sleep(1.0)
 
 
